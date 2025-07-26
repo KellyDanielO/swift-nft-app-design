80960e466b842fbdd214cff5ac431aeebcb20187
KellyDanielO <kizdver@gmail.com>
2025-07-26 14:37:22 +0100

    Initial Commit

---
+++ nftappdesign/nftappdesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  nftappdesignApp.swift
+//  nftappdesign
+//
+//  Created by  macbook    on 26/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct nftappdesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
